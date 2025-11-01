4bafc6ca255e80d2397682a66a686a1a7d3b3016
tharunS123 <tharunsenthilkumar308@gmail.com>
2025-10-31 20:18:01 -0400

    Initial Commit

---
+++ NoteTaking/NoteTakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingApp.swift
+//  NoteTaking
+//
+//  Created by Tharun Senthilkumar on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
